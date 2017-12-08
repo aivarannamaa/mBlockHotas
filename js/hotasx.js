d191a76df5f554b8d2e6a6702c0986eaf1dfb7ad
Aivar Annamaa <aivar.annamaa@gmail.com>
2017-12-08 18:04:49 +0200

    Using Gamepad API

--- js/hotasx.js
+++ js/hotasx.js
@@ -1,76 +1,56 @@
-// joystickExtension.js
-// Shane M. Clements, November 2013
-// Joystick Scratch Extension
-//
-// This is an extension for development and testing of the Scratch Javascript Extension API.
-
 new (function() {
-    var device = null;
-    var input = null;
-    var poller = null;
     var ext = this;
-
+    
     ext._deviceConnected = function(dev) {
-        if(device) return;
-
-        device = dev;
-        device.open();
-
-        poller = setInterval(function() {
-            input = device.read(48);
-        }, 10);
-
-//        setInterval(function() { console.log(input); }, 100);
+        // TODO: 
     };
 
     ext._deviceRemoved = function(dev) {
-        if(device != dev) return;
-        device = null;
-        stopPolling();
+        // TODO: 
     };
 
-    function stopPolling() {
-        if(poller) clearInterval(poller);
-        poller = null;
-    }
-
     ext._shutdown = function() {
-        if(poller) clearInterval(poller);
-        poller = null;
-
-        if(device) device.close();
-        device = null;
+        // TODO: 
     }
 
     ext._getStatus = function() {
-        if(!device) return {status: 1, msg: 'Controller disconnected'};
-        return {status: 2, msg: 'Controller connected'};
+        // TODO: 
+        return {status: 2, msg: "Controller connected"};
     }
 
     // Converts a byte into a value of the range -1 -> 1 with two decimal places of precision
-    function convertByteStr(byte) { return (parseInt(byte, 16) - 128) / 128; }
     ext.readJoystick = function(name) {
-        var retval = null;
-        switch(name) {
-            case 'leftX': retval = convertByteStr(input[12] + input[13]); break;
-            case 'leftY': retval = -convertByteStr(input[14] + input[15]); break;
-            case 'rightX': retval = convertByteStr(input[16] + input[17]); break;
-            case 'rightY': retval = -convertByteStr(input[18] + input[19]); break;
+        // TODO:
+        // http://beej.us/blog/data/javascript-gamepad/
+        var pads = navigator.getGamepads(); 
+        console.log(pads);
+        var pad = pads[0];
+        console.log(pad.axes);
+        var result = 0;
+        if (!pad) {
+            
+        } 
+        else if (name == "X") {
+            result = pad.axes[0];
         }
-
-        // If it's hardly off center then treat it as centered
-        if(Math.abs(retval) < 0.1) retval = 0;
-
-        return retval.toFixed(2);
+        else if (name == "Y") {
+            result = pad.axes[1];
+        }
+        
+        if (Math.abs(result) < 0.01) {
+            return 0;
+        } else {
+            return result;
+        }
     }
 
     var descriptor = {
         blocks: [
-            ['r', 'get joystick %m.joystickPart', 'readJoystick', 'leftX']
+            ["r", "get joystick %m.joystickPart", "readJoystick", "X"]
         ],
         menus: {
-            joystickPart: ['leftX', 'leftY', 'rightX', 'rightY']
+            joystickPart: ["X", "Y"]
         }
     };
-    ScratchExtensions.register('Joystick', descriptor, ext, {type: 'hid', vendor:0x044F, product:0xB108});
+    ScratchExtensions.register("HotasX", descriptor, ext, {type: "hid", vendor:0x044F, product:0xB108});
 })();
